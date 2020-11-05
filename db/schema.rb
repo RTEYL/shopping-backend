3f6c2a858a204b125b6b44a2078439b18370eead
RTEYL <tyler-w-jones@hotmail.com>
2020-11-05 15:14:39 -0800

    add images to items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_04_221908) do
+ActiveRecord::Schema.define(version: 2020_11_05_224855) do
 
   create_table "items", force: :cascade do |t|
     t.string "category"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_11_04_221908) do
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,12 @@
 require 'faker'
-
+  Item.destroy_all
 20.times do
   Item.create(
     brand: Faker::Company.name,
     category: Faker::Commerce.department(max: 2),
     name: Faker::Commerce.product_name,
     description: Faker::Lorem.paragraph(sentence_count: 2),
-    price: Faker::Commerce.price
+    price: Faker::Commerce.price,
+    image: Faker::LoremFlickr.image(size: "300x300", search_terms: ['product'])
   )
 end
