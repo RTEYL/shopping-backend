054394fe26f82198cec2e1a1798a8314a18f4b5e
RTEYL <tyler-w-jones@hotmail.com>
2020-11-12 15:32:25 -0800

    adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :items
+      resources :users, only: [:index, :show]
     end
   end
-  resources :sessions, only: [:create]
-  resources :registrations, only: [:create]
-  delete :logout, to: 'sessions#logout'
-  get :loggend_in, to: 'sessions#logged_in'
+  post :sign_up, to: 'registrations#create'
+  post :login, to: 'sessions#create'
+  post :logout, to: 'sessions#logout'
+  get :logged_in, to: 'sessions#logged_in'
   root to: 'static#home'
 end
