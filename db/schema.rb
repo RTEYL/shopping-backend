717eb30a744fe90413f35c569ae0ad12eb9e090b
RTEYL <tyler-w-jones@hotmail.com>
2020-11-11 17:20:00 -0800

    create user resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_05_224855) do
+ActiveRecord::Schema.define(version: 2020_11_12_000803) do
 
   create_table "items", force: :cascade do |t|
     t.string "category"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2020_11_05_224855) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
