e9585e81a7dac721d820cce12fe70ffecff8f028
RTEYL <tyler-w-jones@hotmail.com>
2020-11-11 17:18:12 -0800

    config api for custion authentication

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,19 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:3000'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+  allow do
+    origins 'http://localhost:5000'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
