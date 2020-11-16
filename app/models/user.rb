ba3fad6a4e6c2e89245aa2ef06334398ef717433
RTEYL <tyler-w-jones@hotmail.com>
2020-11-15 16:00:42 -0800

    increase password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true
   validates :email, uniqueness: true
+  validates :password,
+   :presence => true,
+   :confirmation => true,
+   :length => {:within => 6..15}
 
 end
