4fbe6560ba2c7a6024d5424e447a03eb4d78f64e
RTEYL <tyler-w-jones@hotmail.com>
2020-11-17 20:49:44 -0800

    debugging sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,3 @@
 Rails.application.config.session_store :cookie_store, key: "_shopping_app"
+Rails.application.config.session_store :cookie_store, expire_after: 14.days
+Rails.application.config.session_store :cookie_store, httponly: true
