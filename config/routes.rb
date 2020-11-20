79b221902b6240f5c40ca9bb42f0b217ddc32c57
RTEYL <tyler-w-jones@hotmail.com>
2020-11-20 14:15:53 -0800

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :items
-      resources :users, only: [:index, :show]
+      resources :users do
+      resources :orders, only: [:create, :show]
+      end
     end
   end
-  resources :users do
-    resources :orders, only: [:create, :show]
-  end
+  post 'guest/orders', to: 'api/v1/orders#create'
   post :sign_up, to: 'registrations#create'
   post :login, to: 'sessions#create'
   post :logout, to: 'sessions#logout'
