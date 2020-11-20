7ef20c1deb02fc6b07142771dad789452ab4aa47
RTEYL <tyler-w-jones@hotmail.com>
2020-11-19 18:27:10 -0800

    add nested routes for user_orders

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
       resources :users, only: [:index, :show]
     end
   end
+  resources :users do
+    resources :orders, only: [:create, :show]
+  end
   post :sign_up, to: 'registrations#create'
   post :login, to: 'sessions#create'
   post :logout, to: 'sessions#logout'
