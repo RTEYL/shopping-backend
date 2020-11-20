a5195ab95da5b9d39e7aaaf790b352bb3d2c88f9
RTEYL <tyler-w-jones@hotmail.com>
2020-11-20 15:23:37 -0800

    rendering json

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,12 +1,20 @@
 class Api::V1::OrdersController < ApplicationController
 
   def  create
-    byebug
     user = User.find_by_id(order_params[:user_id])
     if user && user.id === current_user.id
+      byebug
       order = user.orders.build(order_params)
+    elsif user.id !== current_user.id
+      render json: {status: 401}
     else
       order = Orders.create(order_params)
+    end
+
+    if order.save
+      render json: {status: 201, order: order}
+    else
+      render json: {status: 400, errors: 'Order was unable to be processed'}
     end
 
   end
