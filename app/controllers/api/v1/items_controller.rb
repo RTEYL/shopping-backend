c6d2c75e4d3d6524eb36979d6a6f4fe167c222da
RTEYL <tyler-w-jones@hotmail.com>
2020-11-25 18:09:20 -0800

    develop items controller to work with admin

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,7 +1,48 @@
 class Api::V1::ItemsController < ApplicationController
 
     def index
-      @items = Item.all
-      render json: @items
+      items = Item.all
+      render json: items
+    end
+
+    def create
+      byebug
+      item= Item.create(params)
+      if item.save
+        render json: {status: 201, errors: "item was updated successfully"}
+      else
+        render json: {status: 400, errors: item.errors.full_messages}
+      end
+    end
+
+    def show
+      byebug
+      item = Item.find_by_id(params[:id])
+      if item
+        render json: {status: 200, item: item}
+      else
+        render json: {status: 400, errors: 'item not found'}
+      end
+    end
+
+    def update
+      byebug
+      item = Item.find_by_id(params[:id])
+      item.update(params)
+      if item.save
+        render json: {status: 201, errors: "item was updated successfully"}
+      else
+        render json: {status: 400, errors: "server was unable to update the item"}
+      end
+    end
+
+    def destroy
+      item = Item.find_by_id(params[:id])
+      item.destroy
+      if item.destroyed?
+        render json: {status: 201, errors: "item was removed form database"}
+      else
+        render json: {status: 400, errors: "server request failed to remove item"}
+      end
     end
 end
