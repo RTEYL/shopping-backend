453325458134b4392ed20759276940a4a2dce972
RTEYL <tyler-w-jones@hotmail.com>
2020-11-27 20:58:22 -0800

    add admin helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
 
-  helper_method :current_user
+  helper_method :current_user, :isAdmin?
 
     def current_user
-      @current_user ||= User.find_by_id(session[:user])
+      @current_user ||= User.find_by_id(session[:user_id])
+    end
+
+    def isAdmin?
+      current_user.admin
+    end
+
+    def validate_admin
+      if !isAdmin?
+        render json: {status: 401, errors: 'unauthorized access'}
+      end
     end
 end
