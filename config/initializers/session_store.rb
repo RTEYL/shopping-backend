0f3d682dcce7b41ee3bef4e039f46c46d64c91de
RTEYL <tyler-w-jones@hotmail.com>
2021-01-06 16:18:13 -0800

    update session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
-Rails.application.config.session_store :cookie_store, key: "_shopping_app_session", domain: :all
+Rails.application.config.session_store :cookie_store, key: "_shopping_app_session", secure: true
 Rails.application.config.session_store :cookie_store, expire_after: 14.days
 Rails.application.config.session_store :cookie_store, httponly: true
+Rails.application.config.session_store :cookie_store, domain: :all
