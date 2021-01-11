1a05f6fed76b7c9bbfee1f325cd3f8029ab7ca43
RTEYL <tyler-w-jones@hotmail.com>
2021-01-10 19:00:58 -0800

    prod persistance bug

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,5 +3,10 @@ Rails.application.config.session_store :cookie_store, {
   expire_after: 14.days,
   cookie_only: true,
   httponly: true,
+<<<<<<< HEAD
   domain: '.devtylerjones.com',
+=======
+  domain: :all,
+  secure: Rails.env.production?
+>>>>>>> 1f2bcfc0cb6fbf2c0b53fe7b2db8d59e879682b4
 }
