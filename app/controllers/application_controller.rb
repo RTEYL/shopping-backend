a77775f9457d2515c2233de06c5f23edce1faa5b
RTEYL <tyler-w-jones@hotmail.com>
2021-01-14 21:12:17 -0800

    update env to work in prod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   helper_method :encode_token, :auth_header, :decoded_token, :current_user, :isAdmin?, :validate_admin
 
-  SECRET = Rails.application.secrets.secret_key_base
+  SECRET = ENV['SECRET_KEY_BASE'] || Rails.application.secrets.secret_key_base
 
   def encode_token(payload)
     JWT.encode(payload, SECRET)
